b0d39dbdcd97fa4addade4d935300b2dae7f6487
Allan Boswell <144160365+allboswe@users.noreply.github.com>
2024-03-04 00:04:37 -0600

    Update src/index.js
    Signed-off-by: Allan Boswell <144160365+allboswe@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 "use strict";
 
 require("./utils/dotenv");
+
+const { Winston } = require("./utils/winston");
+const { Mongoose } = require("./utils/mongoose");
+
+const { Application } = require("./client/discord");
+
+const winston = new Winston();
+const mongoose = new Mongoose();
+
+const application = new Application(winston.container, mongoose.container);
