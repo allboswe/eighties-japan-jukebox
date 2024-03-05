e843df7472ac27334883ee8a779ef3ed1587ec4b
Allan Boswell <144160365+allboswe@users.noreply.github.com>
2024-03-05 00:25:43 -0600

    Update src/client/discord.js
    Signed-off-by: Allan Boswell <144160365+allboswe@users.noreply.github.com>

--- src/client/discord.js
+++ src/client/discord.js
@@ -1,5 +1,8 @@
 "use strict";
 
+const fs = require("node:fs");
+const path = require("node:path");
+
 const Client = require("discord.js").Client;
 const GatewayIntentBits = require("discord.js").GatewayIntentBits;
 
@@ -8,6 +11,32 @@ class Application {
     this.client = new Client({ intents: [GatewayIntentBits.Guilds] });
     this.client.winston = winston;
     this.client.mongoose = mongoose;
+
+    this.#registerEvents();
+  }
+
+  #registerEvents() {
+    const eventsPath = path.join(__dirname, "events");
+    const eventFiles = fs
+      .readdirSync(eventsPath)
+      .filter((file) => file.endsWith(".js"));
+
+    for (const file of eventFiles) {
+      const filePath = path.join(eventsPath, file);
+      const event = require(filePath);
+
+      if (event.once)
+        this.client.once(event.name, (...args) => event.execute(...args));
+      else this.client.on(event.name, (...args) => event.execute(...args));
+    }
+  }
+
+  async connect() {
+    return await this.client.login(process.env.DISCORD_TOKEN);
+  }
+
+  async disconnect() {
+    return await this.client.destroy();
   }
 }
 
