c90bd236cdbffaeb7b4d16a5d0252d0b4f7332ee
Allan Boswell <144160365+allboswe@users.noreply.github.com>
2024-03-05 00:25:12 -0600

    Update src/index.js
    Signed-off-by: Allan Boswell <144160365+allboswe@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 "use strict";
 
+/** @throws {Error} ENOENT: if no such file or directory exists. */
 require("./utils/dotenv");
 
 const { Winston } = require("./utils/winston");
 const { Mongoose } = require("./utils/mongoose");
-
 const { Application } = require("./client/discord");
 
 const winston = new Winston();
 const mongoose = new Mongoose();
+const application = new Application(winston.container, mongoose);
 
-const application = new Application(winston.container, mongoose.container);
+process.on("SIGINT", function () {
+  winston.container.warn("`SIGINT` detected.");
+  application.disconnect().then(() => {
+    winston.container.info("Successfully disconnected from Discord.");
+    mongoose.disconnect().then(() => {
+      winston.container.info("Successfully disconnected from MongoDB Atlas.");
+    });
+  });
+});
+
+mongoose.connect().then(() => {
+  winston.container.info("Successfully connected to MongoDB Atlas.");
+  application.connect();
+});
