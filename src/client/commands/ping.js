4e695d745f903f33ba5f879ba4eef0d2618b866c
Allan Boswell <144160365+allboswe@users.noreply.github.com>
2024-03-07 20:36:00 -0600

    Update src/client/commands/ping.js
    Signed-off-by: Allan Boswell <144160365+allboswe@users.noreply.github.com>

--- src/client/commands/ping.js
+++ src/client/commands/ping.js
@@ -1,11 +1,46 @@
 "use strict";
 
+const EmbedBuilder = require("discord.js").EmbedBuilder;
 const SlashCommandBuilder = require("discord.js").SlashCommandBuilder;
+
+function pingEmbed(ws, rtt) {
+  return new EmbedBuilder()
+    .setColor("Green")
+    .setTitle("Ping")
+    .addFields(
+      { name: "Round-Trip Latency", value: `${rtt}ms`, inline: true },
+      { name: "Websocket Heartbeat", value: `${ws}ms`, inline: true },
+    )
+    .setDescription(`Measures the round-trip latency and websocket heartbeat.`);
+}
+
+function waitEmbed() {
+  return new EmbedBuilder()
+    .setColor("Default")
+    .setTitle("Ping")
+    .addFields(
+      { name: "Round-Trip Latency", value: "0ms", inline: true },
+      { name: "Websocket Heartbeat", value: "0ms", inline: true },
+    )
+    .setDescription("Measures the round-trip latency and websocket heartbeat.");
+}
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("ping")
     .setDescription("Measures the round-trip latency and websocket heartbeat."),
   global: false,
-  async execute(interaction) {},
+  async execute(interaction) {
+    const ws = interaction.client.ws.ping;
+    const sent = await interaction.reply({
+      embeds: [waitEmbed()],
+      ephemeral: false,
+      fetchReply: true,
+    });
+
+    const rtt = sent.createdTimestamp - interaction.createdTimestamp;
+    const embeds = [pingEmbed(ws, rtt)];
+
+    return interaction.editReply({ embeds: embeds });
+  },
 };
